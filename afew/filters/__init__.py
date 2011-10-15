f19a01527c2547e988b8f4532ecd518ba2eb76cd
Justus Winter <4winter@informatik.uni-hamburg.de>
2011-10-15 03:54:04 +0200

    avoid globbing on the __init__.py file

--- afew/filters/__init__.py
+++ afew/filters/__init__.py
@@ -19,4 +19,6 @@
 import os
 import glob
 
-__all__ = list(filename[:-3] for filename in glob.glob1(os.path.dirname(__file__), '*.py'))
+__all__ = list(filename[:-3]
+               for filename in glob.glob1(os.path.dirname(__file__), '*.py')
+               if filename is not '__init__.py')
