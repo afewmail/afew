d3dd32b4f014829d8629f2e76ad1161785a584a7
Justus Winter <4winter@informatik.uni-hamburg.de>
2011-12-12 18:19:52 +0100

    py3k: Add a py2.7 compatibility hack

--- afew/filters/__init__.py
+++ afew/filters/__init__.py
@@ -17,9 +17,12 @@ from __future__ import print_function, absolute_import, unicode_literals
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
 
+import sys
 import os
 import glob
 
 __all__ = list(filename[:-3]
-               for filename in glob.glob1(os.path.dirname(__file__), '*.py')
+               for filename in glob.glob1(os.path.dirname(__file__),
+                                          # py2.7 compat hack
+                                          b'*.py' if sys.version_info[0] == 2 else '*.py')
                if filename is not '__init__.py')
