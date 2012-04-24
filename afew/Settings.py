afewmail/afew

commit 428f89423752ddb144b3529a2436100a974b2913

Some stylistic fixes, remove unused imports and variables (pyflakes)

afew/Settings.py
@@ -19,16 +19,10 @@ from __future__ import print_function, absolute_import, unicode_literals
 
 import os
 import re
-import functools
-from collections import OrderedDict
+import collections
 
 from .configparser import SafeConfigParser
 from .Filter import all_filters, register_filter
-
-# config values for the MailMover
-mail_mover = 'MailMover'
-move_folders = 'folders'
-move_age = 'max_age'
 
 settings = SafeConfigParser()
 # preserve the capitalization of the keys.
@@ -42,7 +36,7 @@ settings.read(os.path.join(os.environ.get('XDG_CONFIG_HOME',
 'All the values for keys listed here are interpreted as ';'-delimited lists'
 value_is_a_list = ['tags']
 
-section_re = re.compile(r'''^(?P<name>[a-z_][a-z0-9_]*)(\((?P<parent_class>[a-z_][a-z0-9_]*)\)|\.(?P<index>\d+))?$''', re.I)
+section_re = re.compile(r'^(?P<name>[a-z_][a-z0-9_]*)(\((?P<parent_class>[a-z_][a-z0-9_]*)\)|\.(?P<index>\d+))?$', re.I)
 def get_filter_chain():
     filter_chain = []
 
@@ -64,7 +58,7 @@ def get_filter_chain():
         if match.group('parent_class'):
             try:
                 parent_class = all_filters[match.group('parent_class')]
-            except KeyError as e:
+            except KeyError:
                 raise NameError('Parent class %r not found in filter type definition %r.' % (match.group('parent_class'), section))
 
             new_type = type(match.group('name'), (parent_class, ), kwargs)
@@ -72,25 +66,24 @@ def get_filter_chain():
         else:
             try:
                 klass = all_filters[match.group('name')]
-            except KeyError as e:
+            except KeyError:
                 raise NameError('Filter type %r not found.' % match.group('name'))
 
             filter_chain.append(klass(**kwargs))
 
     return filter_chain
 
-
+mail_mover_section = 'MailMover'
 def get_mail_move_rules():
-    rule_pattern = re.compile("'(.+?)':(\S+)")
-    query_target_pattern = re.compile("")
-    if settings.has_option(mail_mover, move_folders):
-        all_rules = OrderedDict()
+    rule_pattern = re.compile(r"'(.+?)':(\S+)")
+    if settings.has_option(mail_mover_section, 'folders'):
+        all_rules = collections.OrderedDict()
 
-        for folder in settings.get(mail_mover, move_folders).split():
-            if settings.has_option(mail_mover, folder):
-                rules = OrderedDict()
+        for folder in settings.get(mail_mover_section, 'folders').split():
+            if settings.has_option(mail_mover_section, folder):
+                rules = collections.OrderedDict()
                 raw_rules = re.findall(rule_pattern,
-                                       settings.get(mail_mover, folder))
+                                       settings.get(mail_mover_section, folder))
                 for rule in raw_rules:
                     rules[rule[0]] = rule[1]
                 all_rules[folder] = rules
@@ -101,9 +94,8 @@ def get_mail_move_rules():
     else:
         raise NameError("No folders defined to move mails from.")
 
-
 def get_mail_move_age():
     max_age = 0
-    if settings.has_option(mail_mover, move_age):
-        max_age = settings.get(mail_mover, move_age)
+    if settings.has_option(mail_mover_section, 'max_age'):
+        max_age = settings.get(mail_mover_section, 'max_age')
     return max_age

afew/Filter.py
@@ -98,7 +98,7 @@ class Filter(Database):
                     db = notmuch.Database(self.db_path,
                                           mode = notmuch.Database.MODE.READ_WRITE)
                     break
-                except notmuch.NotmuchError as e:
+                except notmuch.NotmuchError:
                     time_left = int(retry_for - (time.time() - start_time))
 
                     if time_left <= 0:

afew/main.py
@@ -23,7 +23,6 @@ import random
 from .Database import Database
 from .DBACL import DBACL as Classifier
 from .MailMover import MailMover
-from .NotmuchSettings import notmuch_settings
 from .utils import extract_mail_body
 
 def main(options, query_string):

commit a3ea63d7048faedb6cc58b4abcb6d4ecfddfb5db

Fix a bug introduced in 428f89423752ddb144b3529a2436100a974b2913

afew/Settings.py
@@ -35,13 +35,14 @@ settings.read(os.path.join(os.environ.get('XDG_CONFIG_HOME',
 
 'All the values for keys listed here are interpreted as ';'-delimited lists'
 value_is_a_list = ['tags']
+mail_mover_section = 'MailMover'
 
 section_re = re.compile(r'^(?P<name>[a-z_][a-z0-9_]*)(\((?P<parent_class>[a-z_][a-z0-9_]*)\)|\.(?P<index>\d+))?$', re.I)
 def get_filter_chain():
     filter_chain = []
 
     for section in settings.sections():
-        if section == 'global' or section == mail_mover:
+        if section == 'global' or section == mail_mover_section:
             continue
 
         match = section_re.match(section)
@@ -73,7 +74,6 @@ def get_filter_chain():
 
     return filter_chain
 
-mail_mover_section = 'MailMover'
 def get_mail_move_rules():
     rule_pattern = re.compile(r"'(.+?)':(\S+)")
     if settings.has_option(mail_mover_section, 'folders'):