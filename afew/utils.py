45d69d23fb42857b7295c465259dd0c001963f61
Justus Winter <4winter@informatik.uni-hamburg.de>
2012-05-08 16:36:21 +0200

    Robustness fix: replace invalid characters when decoding messages
    Fixes a regression introduced by b58ac76 when using python < 3.2.
    
    Closes #34.

--- afew/utils.py
+++ afew/utils.py
@@ -88,7 +88,7 @@ def extract_mail_body(message):
     if hasattr(email, 'message_from_binary_file'):
         mail = email.message_from_binary_file(open(message.get_filename(), 'br'))
     else:
-        fp = codecs.open(message.get_filename(), 'r', 'utf-8', errors='ignore')
+        fp = codecs.open(message.get_filename(), 'r', 'utf-8', errors='replace')
         mail = email.message_from_file(fp)
 
     content = []
