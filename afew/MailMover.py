d8bc5bd6bf4615be12b7a7359dd8ea25d3028059
Chris Fleming <me@chrisfleming.org>
2013-04-18 14:56:32 +0200

    Added utf8 encode to logger.
    when running: afew --move-mails -n
    
    I was getting the error:
    Traceback (most recent call last):
      File "/home/chrisfl/.local/bin/afew", line 189, in <module>
        main(options, query_string)
      File "/home/chrisfl/.local/lib/python2.7/site-packages/afew/main.py", line 73, in main
        mover.move(maildir, rules)
      File "/home/chrisfl/.local/lib/python2.7/site-packages/afew/MailMover.py", line 62, in move
        self.__log_move_action(message, maildir, rules[query], self.dry_run)
      File "/home/chrisfl/.local/lib/python2.7/site-packages/afew/MailMover.py", line 103, in __log_move_action
        logging.log(level, "    {}".format(get_message_summary(message)))
    UnicodeEncodeError: 'ascii' codec can't encode character u'\xa3' in position 58: ordinal not in range(128)
    
    This fixes it.

--- afew/MailMover.py
+++ afew/MailMover.py
@@ -100,7 +100,7 @@ class MailMover(Database):
             level = logging.INFO
             prefix = 'I would move mail'
         logging.log(level, prefix)
-        logging.log(level, "    {}".format(get_message_summary(message)))
+        logging.log(level, "    {}".format(get_message_summary(message).encode('utf8')))
         logging.log(level, "from '{}' to '{}'".format(source, destination))
         #logging.debug("rule: '{}' in [{}]".format(tag, message.get_tags()))
 
