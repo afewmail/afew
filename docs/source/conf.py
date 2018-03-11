0fc75fcdc2c1762a9875b0e53707fa99c7d44819
Sebastian Schwarz <seschwar@gmail.com>
2018-03-11 18:02:00 +0100

    doc: fix path to source code
    When being run in an environment where afew was not already
    installed Sphinx would print the following warnings:
    
        docs/source/implementation.rst:11: WARNING: autodoc: failed to import class u'Database' from module u'afew.Database'; the following exception was raised:
        docs/source/implementation.rst:18: WARNING: autodoc: failed to import class u'Filter' from module u'afew.Filter'; the following exception was raised:
        docs/source/implementation.rst:24: WARNING: autodoc: failed to import module u'afew.Settings'; the following exception was raised:
        docs/source/implementation.rst:27: WARNING: autodoc: failed to import module u'afew.NotmuchSettings'; the following exception was raised:
        docs/source/implementation.rst:34: WARNING: autodoc: failed to import module u'afew.utils'; the following exception was raised:
    
    Sphinx needs to be made aware of the location to the source
    code so it can find the Python modules even when they aren't
    installed already.  This is required when the documentation is
    built in a clean environment such as when creating packages for
    operating systems.
    
    This fixes #184.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # Create mocks so we don't depend on non standard modules to build the
 # documentation
