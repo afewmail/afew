4117ab9f11d062bc5c032076074a54f5b2073057
Johannes Löthberg <johannes@kyriasis.com>
2019-09-28 21:32:35 +0200

    NotmuchSettings: Add write_notmuch_settings
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- afew/NotmuchSettings.py
+++ afew/NotmuchSettings.py
@@ -16,6 +16,13 @@ def read_notmuch_settings(path = None):
 
     notmuch_settings.readfp(open(path))
 
+def write_notmuch_settings(path = None):
+    if path == None:
+        path = os.environ.get('NOTMUCH_CONFIG', os.path.expanduser('~/.notmuch-config'))
+
+    with open(path, 'w+') as fp:
+        notmuch_settings.write(fp)
+
 def get_notmuch_new_tags():
     # see issue 158
     return filter(lambda x: x != 'unread', notmuch_settings.get_list('new', 'tags'))
