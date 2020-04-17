d6100e5e1d8e621459915d3aef3d47e7ca194f92
Amadeusz Piotr Żołnowski <aidecoe@aidecoe.name>
2020-04-17 23:12:52 +0100

    Catch DKIM verification erros and log them as warnings
    Fixes #263.

--- afew/filters/DKIMValidityFilter.py
+++ afew/filters/DKIMValidityFilter.py
@@ -1,9 +1,23 @@
 # SPDX-License-Identifier: ISC
 # Copyright (c) Amadeusz Zolnowski <aidecoe@aidecoe.name>
 
+"""
+DKIM validator filter.
+
+Verifies DKIM signature of an e-mail which has DKIM header.
+"""
+
+import logging
+
 import dkim
+import dns.exception
 
 from afew.filters.BaseFilter import Filter
+
+
+class DKIMVerifyError(Exception):
+    """Failed to verify DKIM signature.
+    """
 
 
 def verify_dkim(path):
@@ -16,7 +30,10 @@ def verify_dkim(path):
     with open(path, 'rb') as message_file:
         message_bytes = message_file.read()
 
-    return dkim.verify(message_bytes)
+    try:
+        return dkim.verify(message_bytes)
+    except (dns.exception.DNSException, dkim.DKIMException) as exception:
+        raise DKIMVerifyError(str(exception)) from exception
 
 
 class DKIMValidityFilter(Filter):
@@ -29,8 +46,19 @@ class DKIMValidityFilter(Filter):
     def __init__(self, database, ok_tag='dkim-ok', fail_tag='dkim-fail'):
         super().__init__(database)
         self.dkim_tag = {True: ok_tag, False: fail_tag}
+        self.log = logging.getLogger('{}.{}'.format(
+            self.__module__, self.__class__.__name__))
 
     def handle_message(self, message):
         if message.get_header(self.header):
-            dkim_ok = all(map(verify_dkim, message.get_filenames()))
+            try:
+                dkim_ok = all(map(verify_dkim, message.get_filenames()))
+            except DKIMVerifyError as verify_error:
+                self.log.warning(
+                    "Failed to verify DKIM of '%s': %s "
+                    "(marked as 'dkim-fail')",
+                    message.get_message_id(),
+                    verify_error
+                )
+                dkim_ok = False
             self.add_tags(message, self.dkim_tag[dkim_ok])
