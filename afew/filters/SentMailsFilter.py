45c54aa97f243304b5be7ffc6d7b0100936ac972
Tim Quelch <tim@quelch.name>
2020-05-02 12:15:43 +1000

    Revert "Fix trailing ; in notmuch config breaking SentMailsFilter"
    This reverts commit 69edd15602e157003e402dedc3304d5b83515aeb.

--- afew/filters/SentMailsFilter.py
+++ afew/filters/SentMailsFilter.py
@@ -18,8 +18,7 @@ class SentMailsFilter(Filter):
         my_addresses.add(notmuch_settings.get('user', 'primary_email'))
         if notmuch_settings.has_option('user', 'other_email'):
             for other_email in notmuch_settings.get('user', 'other_email').split(';'):
-                if other_email:
-                    my_addresses.add(other_email)
+                my_addresses.add(other_email)
 
         self.query = (
                 '(' +
